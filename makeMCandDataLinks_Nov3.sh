ce06cd8ca8658a6283667b0509cca1ff4714351f
Silvio Donato <silvio.donato@cern.ch>
2016-11-07 11:35:31 +0100

    make links to data samples in T3

---
+++ makeMCandDataLinks_Nov3.sh
@@ -0,0 +1,14 @@
+mkdir -p Nov3
+
+cd Nov3
+
+cp -sf /mnt/t3nfs01/data01/shome/sdonato/tth/gc/projectSkimFH/GC017acd9c0ed1/*root .
+cp -sf /mnt/t3nfs01/data01/shome/sdonato/tth/gc/projectSkimDataFH/GCd57c3b0e2eea/*root .
+
+mkdir -p ../Nov3_lept
+cd ../Nov3_lept/
+
+cp -sf /mnt/t3nfs01/data01/shome/sdonato/tth/gc/projectSkimFH_lepton/GC0c93204d34ca/*root .
+cp -sf /mnt/t3nfs01/data01/shome/sdonato/tth/gc/projectSkimDataFH_lepton/GCc14df5d28d22/*root .
+
+cd ..
