c8be7e4be6a7fdb2effaeb450b00e64e203c7066
Silvio Donato <silvio.donato@cern.ch>
2017-01-10 15:14:36 +0100

    improvements in getROC

--- getROC.py
+++ getROC.py
@@ -1,26 +1,57 @@
+from glob import glob
 import ROOT
 import array
 import os,sys
+
+printLineNull = True
+printLineSignif = True
+printLineSignifN = True
 
 def getROC(background, signal):
     xaxis = background.GetXaxis()
     xaxis_check = signal.GetXaxis()
     assert ( xaxis.GetNbins()==xaxis_check.GetNbins() and xaxis.GetXmax()==xaxis_check.GetXmax() and xaxis.GetXmin()==xaxis_check.GetXmin() )
+    nbins = xaxis.GetNbins()
     
-    bkg_int = background.GetIntegral()
-    sig_int = signal.GetIntegral()
-    sig_eff = []
-    bkg_rej = []
+    bkg_tot = background.Integral(0,nbins+1)
+    sig_tot = signal.Integral(0,nbins+1)
+    ## nbins+3: first bin zero + nbins + overflow + underflow
+    bkg_int = [0]*(nbins+3)
+    sig_int = [0]*(nbins+3)
+    sig_eff = [0]*(nbins+3)
+    bkg_rej = [0]*(nbins+3)
+    for i in range(1,nbins+3):
+        sig_int[i] = (sig_int[i-1]+signal.GetBinContent(i-1))
+        bkg_int[i] = (bkg_int[i-1]+background.GetBinContent(i-1))
+    
+#    ## possible check
+#    print bkg_int[nbins+2],"=",background.Integral(0,nbins+1)
+#    print sig_int[nbins+2],"=",signal.Integral(0,nbins+1)
+    
+    bkg_tot = bkg_int[nbins+2]
+    sig_tot = sig_int[nbins+2]
+
+    bestS2_B = -3
+    bestCut = 0
     inverted = True
-    for i in range(xaxis.GetNbins()+1):
+    for i in range(0,nbins+3):
+        sig_eff[i] = sig_int[i]/sig_tot
+        bkg_rej[i] = 1 - bkg_int[i]/bkg_tot
         if inverted:
-            sig_int[i] = 1 - sig_int[i]
-            bkg_int[i] = 1 - bkg_int[i]
-        sig_eff.append(sig_int[i]) 
-        bkg_rej.append(1.-bkg_int[i]) 
-#        print sig_int[i], bkg_int[i]
-    
-    
+            sig_eff[i] = 1 - sig_eff[i]
+            bkg_rej[i] = 1 - bkg_rej[i]
+        ## find the best cuts (highest eff/sqrt(1-rej) or eff**2/(1-rej))
+        S2_B = sig_eff[i]*sig_eff[i]/(1.0001-bkg_rej[i])
+        if S2_B>bestS2_B:
+            bestCut = xaxis.GetBinLowEdge(i)
+            bestS2_B = S2_B
+#        print "sig_eff[i]:",sig_eff[i],
+#        print "\tbkg_rej[i]:",bkg_rej[i]
+#        print "\tvalue:",xaxis.GetBinCenter(i)
+
+    if bestS2_B>1.01:
+        print "Best cut: ",bestCut,"\timprove S/sqrt(B) of: ",(bestS2_B)**0.5,
+#    print "bkg_tot: ",bkg_tot,"\tsig_tot: ",sig_tot,
     arrX, arrY = array.array('f',sig_eff),array.array('f',bkg_rej)
     ROC = ROOT.TGraph( len(sig_eff) , arrX, arrY )
     ROC.SetLineColor(ROOT.kRed)
@@ -29,7 +60,7 @@ def getROC(background, signal):
     ROC.GetXaxis().SetTitle("Signal efficiency")
     ROC.GetYaxis().SetTitle("Background rejection")
     
-    return ROC
+    return ROC,bestS2_B
 
 def doROC(fileName):
     print "I'm doing ",fileName
@@ -39,11 +70,32 @@ def doROC(fileName):
     stack = list_[1]
     background = stack.GetStack().Last().Clone("background")
     signal = list_[2]
-    roc = getROC(background,signal)
+    roc,bestS2_B = getROC(background,signal)
     c2 = ROOT.TCanvas()
     c2.SetGridx()
     c2.SetGridy()
-    roc.Draw("APL")
+    roc.Draw("AP")
+    
+    if printLineNull:
+        lineNull = ROOT.TF1("lineNull","1-x",0,1)
+        lineNull.SetLineColor(ROOT.kBlack)
+        lineNull.SetLineStyle(2)
+        lineNull.Draw("same")
+    
+    if printLineSignif:
+        lineSignif = ROOT.TF1("lineSignif","1-x**2",0,1)
+        lineSignif.SetLineColor(ROOT.kBlack)
+        lineSignif.SetLineStyle(2)
+        lineSignif.Draw("same")
+    
+    if printLineSignifN:
+        N = (bestS2_B)**0.5
+        lineSignifN = ROOT.TF1("lineSignifN","1-(x/%s)**2"%N,0,1)
+        lineSignifN.SetLineColor(ROOT.kBlack)
+        lineSignifN.SetLineStyle(2)
+        lineSignifN.Draw("same")
+
+    roc.Draw("L")
     folder = fileName.split("/")[:-1]
     folder[-1] = "ROC"
     folder = '/'.join(folder)
@@ -65,7 +117,10 @@ if __name__ == "__main__":
     print "Input files: ", inputFiles
     
     for inputFile in inputFiles:
-        doROC(inputFile)
+        files = glob(inputFile) ## solve possible wildcard characters
+        for file_ in files:
+            doROC(file_)
+            print "\n"
 
 
 #    doROC("plotsDec24/SumCSVnjetsbtagCSV1IterationLength.root")
